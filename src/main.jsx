64ec027ea8b9e429fc581446ce439a46167317e0
Antonio <santiagodelarosaantonio@gmail.com>
2023-11-22 00:04:47 +0100

    Add .env file and set MUI license key

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
 import Providers from './providers/Providers';
+import { LicenseInfo } from '@mui/x-license-pro';
 
 import './index.css'
+
+LicenseInfo.setLicenseKey(import.meta.env.VITE_MUI_LICENSE);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,6 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App/>
       </Providers>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
 
