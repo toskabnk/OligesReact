c3cf3583555718e829cabd2f44dd78676ed180c3
Antonio <santiagodelarosaantonio@gmail.com>
2023-12-09 02:27:35 +0100

    Added theme - Styles edited for use theme colors

--- src/providers/Providers.jsx
+++ src/providers/Providers.jsx
@@ -2,14 +2,21 @@ import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import theme from "../theme/theme";
+import { ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider as StyledProvider } from "styled-components";
 
 const Providers = ({children}) => {
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Provider store={store}>
-                {children}
-            </Provider>
-        </LocalizationProvider>
+        <StyledProvider theme={theme}>
+            <ThemeProvider theme={theme}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <Provider store={store}>
+                        {children}
+                    </Provider>
+                </LocalizationProvider>
+            </ThemeProvider>
+        </StyledProvider>
     )
 }
 
